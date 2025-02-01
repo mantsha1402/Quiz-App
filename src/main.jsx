0e6ac2418273cdc7a001e993d13faeb8f615767a
mantsha1402 <mantsha.1402@gmail.com>
2025-02-01 18:01:56 +0530

    module3Evaluation

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import { createRoot} from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.render(
+const container= document.getElementById('root');
+const root= createRoot(container);
+
+root.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
